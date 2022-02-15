ce1f95b6a33e94b31f9349c9ba620a60186d610e
durasel74 <diman174cheb@mail.ru>
2022-02-15 21:29:06 +0500

    GUI working

--- EntityFrameworkLearn/VM/VIewModel.cs
+++ EntityFrameworkLearn/VM/VIewModel.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
 using EntityFrameworkLearn.Model;
 
 namespace EntityFrameworkLearn.VM;
@@ -10,17 +11,18 @@ public class ViewModel : INotifyPropertyChanged
 {
     public ViewModel()
     {
-        var db = new AppDbContext();
+       // var db = new AppDbContext();
 
-        //var tables = db.Users_Tours.ToList();
-        var tables = db.Users_Tours.ToList();
-        foreach (var table in tables)
-        {
-            string output = $"{table.userLogin} {table.tourName} " +
-			    $"{table.tourDate}";
-            Console.WriteLine(output);
-        }
+       // var tables = db.Users_Tours.ToList();
+       // foreach (var table in tables)
+       // {
+       //     string output = $"{table.userLogin} {table.tourName} " +
+			    //$"{table.tourDate}";
+       //     Console.WriteLine(output);
+       // }
     }
+
+    public ObservableCollection<String> PagesList { get; set; }
 
     public event PropertyChangedEventHandler PropertyChanged;
     public void OnPropertyChanged([CallerMemberName] string prop = "")

5b25504478dad0653a3446aadbb0b8f045cf3a2e
durasel74 <diman174cheb@mail.ru>
2022-02-16 22:13:21 +0500

    Pages added

---
+++ EntityFrameworkLearn/Model/Page.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EntityFrameworkLearn.Model
+{
+	public class Page
+	{
+		public string PageTitle { get; set; }
+		public int PageId { get; set; }
+
+		public Page(string pageTitle, int pageId) 
+		{
+			PageTitle = pageTitle;
+			PageId = pageId;
+		}
+	}
+}
--- EntityFrameworkLearn/VM/VIewModel.cs
+++ EntityFrameworkLearn/VM/VIewModel.cs
@@ -9,20 +9,40 @@ namespace EntityFrameworkLearn.VM;
 
 public class ViewModel : INotifyPropertyChanged
 {
+    private Page selectedPage;
+
     public ViewModel()
     {
-       // var db = new AppDbContext();
+        // var db = new AppDbContext();
 
-       // var tables = db.Users_Tours.ToList();
-       // foreach (var table in tables)
-       // {
-       //     string output = $"{table.userLogin} {table.tourName} " +
-			    //$"{table.tourDate}";
-       //     Console.WriteLine(output);
-       // }
+        // var tables = db.Users_Tours.ToList();
+        // foreach (var table in tables)
+        // {
+        //     string output = $"{table.userLogin} {table.tourName} " +
+        //$"{table.tourDate}";
+        //     Console.WriteLine(output);
+        // }
+
+        Pages = new ObservableCollection<Page> 
+        { 
+            new Page("GrayPage", 0),
+            new Page("MainPage", 1),
+        };
+        SelectedPage = Pages[0];
     }
 
     public ObservableCollection<String> PagesList { get; set; }
+    public ObservableCollection<Page> Pages { get; set; }
+
+    public Page SelectedPage
+    {
+        get { return selectedPage; }
+        set
+		{
+            selectedPage = value;
+            OnPropertyChanged("SelectedPage");
+		}
+	}
 
     public event PropertyChangedEventHandler PropertyChanged;
     public void OnPropertyChanged([CallerMemberName] string prop = "")
